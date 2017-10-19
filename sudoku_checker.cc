8c723367ebd67a5385fdadaa1e5c9460f5c76030
Don Vu <don.vu1212@gmail.com>
2017-10-18 18:20:41 -0700

    Add parameters for thread support

--- sudoku_checker.cc
+++ sudoku_checker.cc
@@ -14,6 +14,17 @@ using namespace std;
 // Global sudoku array shared by all threads
 int sudoku[9][9];
 
+//  Parameters passed to each thread
+struct parameters {
+	int row;
+	int column;
+};
+
+//  Each thread sets their respective
+//  array index to 1 for valid row/column/square
+//  or 0 for invalid
+int sudokuStatus[11];
+
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		cerr << "Usage: " << argv[0] << " <Sudoku solution file name>" 
